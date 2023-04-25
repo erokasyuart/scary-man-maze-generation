d50c4a3117f706bf30d1dcfa81745a61cc28c7e5
erokasyuart <erikat.stuart@gmail.com>
2023-04-26 08:45:57 +1200

    Added GUI random maze generation

---
+++ Assets/Scripts/MazeConstructor.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+
+public class MazeConstructor : MonoBehaviour
+{
+    public bool showDebug;
+    
+    [SerializeField] private Material mazeMat1;
+    [SerializeField] private Material mazeMat2;
+    [SerializeField] private Material startMat;
+    [SerializeField] private Material treasureMat;
+
+    public int[,] data
+    {
+        get; private set;
+    }
+
+    public int[,] FromDimensions(int sizeRows, int sizeCols)
+    {
+        int[,] maze = new int[sizeRows, sizeCols];
+        
+        int rMax = maze.GetUpperBound(0);
+        int cMax = maze.GetUpperBound(1);
+
+        for (int i = 0; i <= rMax; i++)
+        {        
+            for (int j = 0; j <= cMax; j++)
+            {       
+                if (i == 0 || j == 0 || i == rMax || j == cMax)
+                {               
+                    maze[i, j] = 1;
+                }                                    
+                else if (i % 2 == 0 && j % 2 == 0 && Random.value > placementThreshold)                                    
+                {
+                    maze[i, j] = 1;
+
+                    int a = Random.value < .5 ? 0 : (Random.value < .5 ? -1 : 1);
+                    int b = a != 0 ? 0 : (Random.value < .5 ? -1 : 1);
+                    maze[i+a, j+b] = 1;
+                }
+            } 
+        }
+        return maze;
+    }
+
+    public float placementThreshold = 0.1f; //chance of empty space
+
+    void Awake()
+    {
+        // default to walls surrounding a single empty cell
+        data = new int[,]
+        {
+            {1, 1, 1},
+            {1, 0, 1},
+            {1, 1, 1}
+        };
+    }
+
+    public void GenerateNewMaze(int sizeRows, int sizeCols)
+    {
+        if (sizeRows % 2 == 0 && sizeCols % 2 == 0) //checks if both are even
+        {
+            Debug.LogError("Odd numbers work better for dungeon size.");
+        }
+        data = FromDimensions(sizeRows, sizeCols);
+    }
+
+    void OnGUI()
+    {
+        if (!showDebug)
+            return;
+
+        int[,] maze = data;
+        int rMax = maze.GetUpperBound(0);
+        int cMax = maze.GetUpperBound(1);
+
+        string msg = "";
+
+        for (int i = rMax; i >= 0; i--)
+        {
+            for (int j = 0; j <= cMax; j++)
+                msg += maze[i, j] == 0 ? "...." : "==";
+            msg += "\n";
+        }
+
+        GUI.Label(new Rect(20, 20, 500, 500), msg);
+    }
+}
